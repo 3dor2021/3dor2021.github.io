006a4899fa8f63f8248a157871fdd5a2fcd99cb0
Roberto M Dyke <dykerm@cardiff.ac.uk>
2021-05-28 13:07:13 +0200

    Commit

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -3,7 +3,7 @@
  */
 
 /*var countDownDate = new Date("Apr 2, 2021 00:00:00 UTC-12:00").getTime();  1 minute past 23:59:59 on 01/04/2021 https://time.is/Anywhere_on_Earth */
-var countDownDate = new Date("May 28, 2021 00:00:00 UTC-12:00").getTime(); /* 1 minute past 23:59:59 on 28/05/2021 https://time.is/Anywhere_on_Earth */
+var countDownDate = new Date("May 29, 2021 00:00:00 UTC-12:00").getTime(); /* 1 minute past 23:59:59 on 28/05/2021 https://time.is/Anywhere_on_Earth */
 
 function countdownTimer() {
   var now = new Date().getTime();
